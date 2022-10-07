11085917aff0762147d80099d4bc4334ad697d1b
jp-trabajo <jp-trabajor>
2022-10-07 08:09:57 -0700

    guardar cambios para hacer el pull

--- validacion.js
+++ validacion.js
@@ -1,1 +1,10 @@
-//Haz tú validación en javascript acá
+const tgname = document.querySelector(".formcontato__input");
+console.log(tgname);
+
+const tgboton = document.querySelector(".formcontato__form");
+console.log(tgboton);
+
+tgboton.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    console.log("ejecuto el boton");
+})
